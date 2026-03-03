59dcf2c597fff468909c4d837e4f28a2b2822d5a
nour <nourelhodanasr188@gmail.com>
2026-03-03 03:11:36 +0200

    update loading

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -40,7 +40,7 @@ export function LoadingScreen({ onLoadingComplete }: LoadingScreenProps) {
           <img
             className="text-2xl bg-gradient-to-r from-purple-400 via-pink-400 to-blue-400 bg-clip-text text-transparent"
 
-            src="../public/nour-logo.png"
+            src="nour-logo.png"
             alt="Logo"
           
             style={{ fontWeight: 200 }}
